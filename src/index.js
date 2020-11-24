e786e0c0bd0852668a3d94cdbc220ec0bec19154
Влвдимир <vlvdimir@MacBook-Air-Vlvdimir.local>
2020-11-24 14:39:43 +0200

    deleted BrowserRouter import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { updateDB } from "./dataBase";
 import store from "./redux-store";
